e98c8d401a700570d76e50d28e29a629de0a59ca
nkand84 <nityabhagi@gmail.com>
2019-02-21 14:54:54 -0800

    addint css files

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,21 +1,11 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#ccc`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header className="header-display">
+    <div className="header-main-content">
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
